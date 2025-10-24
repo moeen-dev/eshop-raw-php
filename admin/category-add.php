d9c37654a713b9d29d23fd24800e3fb764bef66d
Moeen Uddin <m.moeen.u9@gmail.com>
2025-10-24 20:21:13 +0600

    Update: create category-add,edit,list page and desing adding page

---
+++ admin/category-add.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include_once 'partials/head.php';
+?>
+
+<body>
+    <div class="container-scroller">
+        <!-- partial:partials/_sidebar.html -->
+        <?php
+        include_once 'partials/sidebar.php';
+        ?>
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+            <!-- partial:partials/_navbar.html -->
+            <?php
+            include_once 'partials/navbar.php';
+            ?>
+            <!-- partial -->
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    <div class="row">
+                        <div class="col-12 grid-margin stretch-card">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Basic form elements</h4>
+                                    <p class="card-description"> Basic form elements </p>
+                                    <form class="forms-sample">
+                                        <div class="form-group">
+                                            <label for="exampleInputName1">Name</label>
+                                            <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>File upload</label>
+                                            <input type="file" name="img[]" class="file-upload-default">
+                                            <div class="input-group col-xs-12">
+                                                <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
+                                                <span class="input-group-append">
+                                                    <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary mr-2">Submit</button>
+                                        <button class="btn btn-dark">Cancel</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- content-wrapper ends -->
+                <!-- partial:partials/_footer.html -->
+                <?php
+                include_once 'partials/footer.php';
+                ?>
+                <!-- partial -->
+            </div>
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <?php
+    include_once 'partials/script.php';
+    ?>
+    <!-- End custom js for this page -->
+</body>
+
+</html>
