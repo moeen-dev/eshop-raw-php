f0b7b449aa9fcbcf5481f45e9664cc65f63ad333
Moeen Uddin <m.moeen.u9@gmail.com>
2025-10-28 20:00:10 +0600

    Update: correcting file name

--- admin/category-add.php
+++ admin/category-add.php
@@ -26,7 +26,7 @@ include_once 'partials/head.php';
                                 <div class="card-body">
                                     <h4 class="card-title">Basic form elements</h4>
                                     <p class="card-description"> Basic form elements </p>
-                                    <form class="forms-sample" method="POST" enctype="multipart/form-data">
+                                    <form class="forms-sample" action="controller/categorycontroller.php" method="POST" enctype="multipart/form-data">
                                         <div class="form-group">
                                             <label for="exampleInputName1">Name</label>
                                             <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
--- admin/partials/sidebar.php
+++ admin/partials/sidebar.php
@@ -20,12 +20,12 @@
                 <span class="menu-icon">
                     <i class="mdi mdi-laptop"></i>
                 </span>
-                <span class="menu-title">Basic UI Elements</span>
+                <span class="menu-title">Category</span>
                 <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="ui-basic">
                 <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="category-add.php">Add Category</a></li>
                     <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
                     <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
                 </ul>
