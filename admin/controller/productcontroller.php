68d30b83d170062f1d193201d5e98d03cfaca3bb
Moeen Uddin <m.moeen.u9@gmail.com>
2025-11-02 22:26:50 +0600

    Update: productcontrolle logic half com

--- admin/controller/productcontroller.php
+++ admin/controller/productcontroller.php
@@ -1,2 +1,32 @@
 <?php
 include_once 'db.php';
+
+if (isset($_POST['submit'])) {
+    // Get form data
+    $productName = trim($_POST['productName']);
+    $productCategory = trim($_POST['productCategory']);
+    $price = trim($_POST['price']);
+    $status = $_POST['status'];
+    $description = trim($_POST['description']);
+
+    // Image file handle
+    $image = rand(100, 999) . '-' . date('mdY') . $_FILES['productImage']['name'];
+    $tmp_name = $_FILES['categoryImage']['tmp_name'];
+
+    $errors = [];
+
+    // Erorr handling
+    if (empty($productName) || empty($productCategory) || empty($price) || empty($status) || empty($description) || empty($_FILES['productImage']['name'])) {
+        $errors = "Every fileds are required!";
+    }
+
+    // If any field blank not upload 
+    if (!empty($errors)) {
+        $_SESSION['toastr'] = [
+            'type' => 'error',
+            'message' => implode(', ', $errors)
+        ];
+        header("Location: ../product-add.php?error=validation");
+        exit();
+    }
+}
